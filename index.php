4e5f8714c1d1a727950ceff18fec96c1b2d303f5
cdi-sofiane <42858371+cdi-sofiane@users.noreply.github.com>
2018-09-04 17:17:46 +0200

    ajout element css
    ajout  de css pour meilleur visibiliter des stocks

--- index.php
+++ index.php
@@ -9,14 +9,32 @@ and open the template in the editor.
         <meta charset="UTF-8">
         <title></title>
     </head>
+    <style>
+        div{
+            padding-left: 1px;
+            padding-right: 1px;
+        }
+        div.stock {
+    /*border: dotted;*/
+    font-family: sans-serif;
+    background-color: lightgreen;
+}
+   div.soldout {
+    /*border: double;*/
+    background-color: lightcoral;
+    font-family: fantasy;
+}
+    </style>
     <body>
         <?php
         $URl_option = '?&js_data=true';
         $URl_pages = '3';
-        $URI = array('men' => 'https://www.hhv.de/shop/en/shoes-men/p:i9nIrJ', 'women' => 'https://www.hhv.de/shop/en/shoes-women/p:57ZJ0z');
+        $URI = array('men' => 'https://www.hhv.de/shop/en/sneakers-men/p:9hB7tT', 'women' => 'https://www.hhv.de/shop/en/sneakers-women/p:DHdi5Y');
+
+//        $URI = array('men' => 'https://www.hhv.de/shop/en/shoes-men/p:i9nIrJ', 'women' => 'https://www.hhv.de/shop/en/shoes-women/p:57ZJ0z');
 
         function addr_selection($URI, $URl_option, $URl_pages) {
-            $URl_page=$URl_pages =='1'?$URl_pages=='':$URl_pages;
+            $URl_page = $URl_pages == '1' ? $URl_pages == '' : $URl_pages;
             $base_hhv = "https://www.hhv.de";
             foreach ($URI as $genre => $value) {
 
@@ -30,22 +48,21 @@ and open the template in the editor.
         }
 
         function stock_out(array $shoes, $URl_pages) {
-            
-            $pages=($URl_pages =="")?'Page = ' . '1':'Page = ' . $URl_pages ;
+
+            $pages = ($URl_pages == "") ? 'Page = ' . '1' : 'Page = ' . $URl_pages;
 //            var_dump($pages);
             $data = array();
             foreach ($shoes as $genre => $v) {
-                echo '<div style=""><br><a> Genre => ' . $genre . '</a><a> '.  $pages. '</a></div><br>';
+                echo '<div style=""><br><a> Genre => ' . $genre . '</a><a> ' . $pages . '</a></div><br>';
                 foreach ($v->page->content as $key => $value) {
                     if ($key == 1) {
 
                         $item_par_pages = [$genre => $value->config->parameters->per];
 
-                        nombres_de_shoes_par_pages($genre,$value,$item_par_pages);
+                        nombres_de_shoes_par_pages($genre, $value, $item_par_pages);
                     }
                 }
             }
-            
         }
 
         function recuperation_pagination($value, $URl_pages, $URl_option) {
@@ -55,27 +72,41 @@ and open the template in the editor.
             return $return->to;
         }
 
-        function nombres_de_shoes_par_pages($genre,$value,$item_par_pages) {
+        function nombres_de_shoes_par_pages($genre, $value, $item_par_pages) {
+            echo '<table style="align-center">';
             foreach ($item_par_pages as $key => $nbr_item) {
+                echo '<table>';
 
+                echo '<tr ">';
+                echo '<th><div>';
+                echo $value->config->seo->headline;
+                echo '</div>';
                 for ($i = 0; $i < $nbr_item; $i++) {
-                    
-//                    echo '<div><img src="' . $value->config->items[$i]->images[0]->big . '" style="width:60px;height:60px;vertical-align: middle;"/>';
-//                    echo ' [' . $i . ']=====>[' . $value->config->items[$i]->id . ']' . print_r($value->config->items[$i]->artist) . '</div><br>';
+
+                    echo '<tr><th><div ><img src="' . $value->config->items[$i]->images[0]->big . '" style="width:60px;height:60px;vertical-align: middle;"/></div></th>';
+                    echo '<th>[' . $value->config->items[$i]->id . ']' . ($value->config->items[$i]->artist) . '';
                     if (isset($value->config->items[$i]->sizeVariants)) {
                         foreach ($value->config->items[$i]->sizeVariants as $k => $v) {
                             if ($v->soldOut) {
                                 $data [$genre][$value->config->items[$i]->id][$v->id] = array('marque' => $value->config->items[$i]->artist, 'pointure' => $v->name, 'rupture' => $v->soldOut);
 
-                                echo '<div>';
-//                                print_r($v->id);
-//                                print_r($v->name);
-                                echo '</div>';
+                                echo '<td><div class="soldout">';
+                                  echo'<span>'.($v->id).':<br></span>';
+                                echo'<span>'.($v->name).'</span>';
+                                echo '</div><br></td>';
+                            }else{
+                              echo '<td><div class="stock">';
+                                echo'<span>'.($v->id).':</span>';
+                                echo'<span>'.($v->name).'</span>';
+                                echo '</div></td>';
                             }
                         }
                     }
+                     echo '</div></th>';
                 }
+               echo'</th></tr>';
             }
+            echo '</table>';
             $data['item_par_page'] = $nbr_item;
 //            header('location : index.php ',$data);
             echo print_r(json_encode($data));
